4f4c10e6afbbcdd66624240fc52f63723d4749ef
Jānis Celms <jaanis.celms@gmail.com>
2020-12-29 10:14:39 +0000

    Add routes
    /api/v1/vielas
    /api/v1/viela/<vielasID>

--- serveris.py
+++ serveris.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
 
+import dati
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "Labrīt!"
+  return "Dati!"
+
+@app.route('/api/v1/vielas')
+def vielas():
+  return jsonify(dati.vielas)
+
+@app.route('/api/v1/viela/<vielasID>')
+def viela(vielasID):
+  viela = 'Nav tadas vielas'
+  for v in dati.vielas:
+    if str(v['id']) == vielasID:
+      viela = v
+
+  return jsonify(viela)
+  
 
 
 if __name__ == "__main__":
